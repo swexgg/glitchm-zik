44e7329ae0e06aa0ed0bb30075929c9748edec67
swexgg <egealtun24@gmail.com>
2023-03-05 20:16:28 +0300

    bot

---
+++ events/distube/searchCancel.js
@@ -0,0 +1,5 @@
+module.exports = async (client, message, query) => {
+    message.channel.send(`31 iptal.`);
+}
+
+// Lrows
---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTAxNjA1MjI5OTkzMzcwMDIxNg.Gj9vHM.BySk5YQPqcym8dvr22gs2ucrQnpSEJVSZ46MxY",  // lrows token
+    PREFIX: process.env.PREFIX || "!", // lrows prefix
+    SAHİP: process.env.SAHİP || "898281297796005888", //lrows id
+}
